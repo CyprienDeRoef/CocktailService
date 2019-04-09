dd475edf945737b7873d0d59e3f588ad2bff1c1d
Cyprien De Roef <c.deroef@outlook.fr>
2019-04-09 09:51:41 +0200

    CocktailsService done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
+import { CocktailService } from './cocktail.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CocktailListComponent,
   ],
   imports: [
     BrowserModule
